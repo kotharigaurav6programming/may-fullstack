5730a3f1765b663db21b0931283e86ef57cedeac
gaurav <kotharigaurav6@gmail.com>
2024-10-26 19:32:57 +0530

    js

---
+++ JavaScript/2 Basic Programs/myScript.js
@@ -0,0 +1,3 @@
+document.write("<br>Hello | This is an example of Javascript");
+        document.write('<br><span style="color:red">Hello | This is an example of Javascript</span>');
+        document.write("<br>Hello | This is an example of Javascript");
