658535a59a12cf4843066cc769698f5731aba240
gauravKothari <kotharigaurav6@gmail.com>
2025-06-14 17:56:21 +0530

    react

--- MERN STACK/React JS/4 Component in Reactjs/src/index.js
+++ MERN STACK/React JS/4 Component in Reactjs/src/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import Banner, { Footer, Header, Navbar } from './test.jsx';
+
+// Class Component
+class Header extends React.Component{
+    render(){
+        return(<>
+            <h1>Header Component</h1>
+            <p>This is Header component created by Functional component</p>
+        </>);
+    }
+}
+class Navbar extends React.Component{
+    render(){
+        return(<>
+            <h1>Navbar Component</h1>
+            <p>This is Navbar component created by Functional component</p>
+        </>);
+    }
+}
+class Banner extends React.Component{
+    render(){
+        return(<>
+            <h1>Banner Component</h1>
+            <p>This is Banner component created by Functional component</p>
+        </>);
+    }
+}
+class Footer extends React.Component{
+    render(){
+        return(<>
+            <h1>Footer Component</h1>
+            <p>This is Footer component created by Functional component</p>
+        </>);
+    }
+}
 
 function MainComponent(){
     return(<>
